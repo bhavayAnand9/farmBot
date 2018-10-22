e1882046461a83dca341bea681e7e9db121f47be
bhavayAnand9 <bhavayanandcse@gmail.com>
2018-10-22 23:22:41 +0530

    Initial commit

---
+++ MLmodel.py
@@ -0,0 +1,42 @@
+import sys
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import pickle
+import pandas as pd
+
+np.set_printoptions(threshold=np.inf)
+
+class TFIDFPredictor:
+    
+    def __init__(self):
+        self.vectorizer = TfidfVectorizer()
+ 
+    def train(self, x,y):
+      
+        self.vectorizer.fit(x,y)
+ 
+    def predict(self, context, utterances):
+        # Convert context and utterances into tfidf vector
+        vector_context = self.vectorizer.transform([context])
+        vector_doc = self.vectorizer.transform(utterances)
+        # The dot product measures the similarity of the resulting vectors
+        result = np.dot(vector_doc, vector_context.T).todense()
+        result = np.asarray(result).flatten()
+        # Sort by top results and return the indices in descending order
+        return result
+    
+# Evaluate TFIDF predictor
+pred = TFIDFPredictor()
+data=pd.read_csv('datasets/datafile.csv')
+pred.train(data['QueryText'],data['KCCAns'])
+a=sys.argv[1]
+b=pred.predict(a,data['QueryText'])
+c=0
+for w in b:
+    if w==np.amax(b):
+        break
+    c+=1
+print(data['KCCAns'][c])
+
+# print("It's from python file MLmodel for debugging it'll always show as it is.")
---
+++ index.js
@@ -0,0 +1,85 @@
+'use strict';
+
+require('dotenv').config()
+
+//uncomment when using dialogflow google api
+// const APIAI_TOKEN = '7fddf38f32a249b38e93b3c9c35a8aa0';
+// const APIAI_SESSION_ID = 'farmer-assistant';
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/views')); // html
+app.use(express.static(__dirname + '/public')); // js, css, images
+
+//uncomment it when pushing to heroku cloud
+// const server = app.listen(process.env.PORT || 5000, () => {
+
+const server = app.listen(5000, ()=> {
+  console.log('Server setting up....');
+  console.log('fetching environment variables...');
+
+  setTimeout(() => {
+    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+  }, 3000)
+});
+
+const io = require('socket.io')(server);
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+// const apiai = require('apiai')(APIAI_TOKEN);
+
+// Web UI
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket) {
+
+  socket.on('chat message', (text) => {
+    console.log('Message: ' + text);
+
+      // Get a reply from API.ai
+
+      // let apiaiReq = apiai.textRequest(text, {
+      //   sessionId: APIAI_SESSION_ID
+      // });
+
+      // apiaiReq.on('response', (response) => {
+      //   let aiText = response.result.fulfillment.speech;
+      //   console.log('Bot reply: ' + aiText);
+      //   socket.emit('bot reply', aiText);
+      // });
+
+
+      let aiText = text;
+
+      if(aiText.includes("weather")){
+        var data = "it's fucking awesome!!"
+        socket.emit('bot reply', data)
+      }
+
+      else{
+        var spawn = require("child_process").spawn;
+  
+        var process = spawn('python', ["./MLmodel.py", aiText] );
+  
+        process.stdout.on('data', function(data) {
+            console.log('Bot reply: ' + data);
+            socket.emit('bot reply', data.toString());
+        });
+      }
+
+    });
+
+
+    // apiaiReq.on('error', (error) => {
+    //   console.log(error);
+    // });
+
+    // apiaiReq.end();
+
+  // });
+});
