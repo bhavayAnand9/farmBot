f0990319a03a639426ddc29484a74e0b227a2212
bhavayAnand9 <bhavayanandcse@gmail.com>
2018-10-23 20:45:11 +0530

    resolving heroku deployment  issues

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + '/public')); // js, css, images
 //uncomment it when pushing to heroku cloud
 // const server = app.listen(process.env.PORT || 5000, () => {
 
-const server = app.listen(5000, ()=> {
+const server = app.listen(process.env.PORT || 5000, ()=> {
   console.log('Server setting up....');
   console.log('fetching environment variables...');
 
